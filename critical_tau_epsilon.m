2394e3c874c6d7ff6a7745d0bdbe94f7867a94ea
thoenselaar <t.a.h.hoenselaar@student.tue.nl>
2018-03-14 13:11:03 +0000

    updated the model in simulink a little bit

--- critical_tau_epsilon.m
+++ critical_tau_epsilon.m
@@ -2,31 +2,40 @@ clear all; warning off;
 tic     % start timing
 
 %% Variables being varied
-a_list = 1:0.5:2;
-tau_list = 0.5:0.5:2;
+a_list = 0.25:0.25:1;
+tau_list = 0.5:0.25:2;
+
+T_THRESHOLD = 1;    % time threshold
+D_THRESHOLD = 4;    % distance threshold
+S_THRESHOLD = 0.5;  % speed threshold
+N = 1000;           % moving average parameter
+
+%% car parameters
+car.v_0 = 120/3.6;
+car.delta = 4;
+car.T = 1.6;
+car.s_0 = 2.0;
+car.a = 1.4;
+car.b = 1.67;
+car.l_avg = 5;
+    
+leader = car;
+follower_1 = car;
+
+leader.x0 = 150;
+leader.v0 = 20;
+
+follower_1.x0 = leader.x0 - leader.l_avg - leader.v0 * follower_1.T - follower_1.s_0;
+follower_1.v0 = 20;
+
 
 %% First iteration
 
 for a_i = 1:length(a_list)
-    a = a_list(a_i);
+    a_factor = a_list(a_i);
     
-    a_max = 1;      % maximum acceleration of the leading car
-    a_min = -0.5;   % minimum acceleration of the leading car
-
-    T_THRESHOLD = 1;   % time threshold
-    D_THRESHOLD = 4;   % distance threshold
-    S_THRESHOLD = 0.5; % speed threshold
-
-    N = 1000; % moving average parameter
-
-    % car parameters
-    v_0 = 120;
-    T = 1.5;
-    s_0 = 2.0;
-    % a = 1.0;   % is being varied
-    b = 2.0;
-    delta = 4;
-    l_avg = 4.8;
+    a_max = 1*a_factor;      % maximum acceleration of the leading car
+    a_min = -0.5*a_factor;   % minimum acceleration of the leading car
 
     fprintf('Simulation %d/%d started \n',a_i,length(a_list))
     sim('car_following_model')
@@ -53,6 +62,7 @@ for a_i = 1:length(a_list)
         delta_epsilon = 0.01;
         tau = tau_list(tau_i);
         epsilon = 0.03;             % guess
+        disp('Low precision');
         run('conformance_check.m')
 
         if result == 0
@@ -72,6 +82,7 @@ for a_i = 1:length(a_list)
         epsilon_raw(a_i,tau_i) = epsilon;
         %% Higher precision conformance checks
         delta_epsilon = 0.001;
+        disp('High precision');
         run('conformance_check.m')
 
         if result == 0
